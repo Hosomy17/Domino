1dd4b6d9a7cb3757356780099afe243929b9b5ea
Jackson Daniel <jackson.daniel.17dh@gmail.com>
2017-09-24 15:56:37 -0400

    Add Title

--- public/js/states/menu.js
+++ public/js/states/menu.js
@@ -4,11 +4,15 @@ State.Menu = function(){
 State.Menu.prototype = {
 	preload : function (){
 		this.load.spritesheet('start', 'assets/sprites/start.png',75,25);
+		this.load.spritesheet('bg', 'assets/sprites/title.png');
 	},
 
 	create : function (){
 		var btn = Game.add.button(Game.world.centerX, Game.world.height * 0.75, 'start', function(){Game.state.start('LoadState')}, this, 1, 0, 2);//<<<<<<<this está ok?
+		var bg = Game.add.sprite(Game.world.centerX, Game.world.centerY, 'bg');
+		bg.anchor.set(0.5);
 		btn.anchor.set(0.5);
+		btn.setScaleMinMax(2);
 	},
 
 	update : function (){
